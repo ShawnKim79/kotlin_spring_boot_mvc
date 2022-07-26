279124b280997065de8f06c53b1a014e9f1c30d0
ShawnKim79 <madsyntst@gmail.com>
2022-07-27 00:22:23 +0900

    add get method, study annotation

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,8 +1,31 @@
 package com.example.mvc.controller.get
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class GetApiController {
+    @GetMapping(path = ["/hello", "/greeting"])
+    fun hello(): String{
+        return "hello kotlin"
+    }
+
+    @RequestMapping(method = [RequestMethod.GET], path = ["request-mapping"])
+    fun requestMapping(): String{
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") _age:Int): String{
+        val name = "kotlin"
+
+        println("${_name}, ${_age}")
+        return _name + " " + _age
+    }
+
 
 }
