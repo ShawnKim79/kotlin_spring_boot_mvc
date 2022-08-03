083af1f666ec83eff0f2c006e04150a371bbb9bd
ShawnKim79 <madsyntst@gmail.com>
2022-08-03 22:00:23 +0900

    add get method. final

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,9 +1,11 @@
 package com.example.mvc.controller.get
 
+import com.example.mvc.model.http.UserRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -24,8 +26,30 @@ class GetApiController {
         val name = "kotlin"
 
         println("${_name}, ${_age}")
-        return _name + " " + _age
+        return "$_name $_age"
     }
 
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam(name = "name") name: String,
+        @RequestParam(value = "age") age: Int
+    ):String {
+        println("${name}, ${age}")
+        return "${name}, ${age}"
+        
+    }
 
+    @GetMapping("get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest{
+        println(userRequest)
+        return userRequest
+    }
+
+    @GetMapping("/get-mapping/query-param/map")
+    fun queryParamMap(@RequestParam map: Map<String, Any>): Map<String, Any>{
+        println(map)
+        val phoneNumber = map.get("phone-number")
+        println(phoneNumber)
+        return map
+    }
 }
